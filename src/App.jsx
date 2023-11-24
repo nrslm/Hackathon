4cc808b208dd5deaef0c101b7a2e20e760373ae0
lev1tha <edik45_strelok@mail.ru>
2023-11-25 01:33:51 +0600

    lev1

--- src/App.jsx
+++ src/App.jsx
@@ -91,60 +91,60 @@ const people = [
 ]
 const peoples = [
   {
-    name: 'Leslie Alexander',
-    role: 'Co-Founder / CEO',
+    name: 'Болотов Мирбек Нарынбекович',
+    role: 'Главный аудитор Управления внутреннего аудита',
+    imageUrl:
+      'https://www.rsk.kg/media/uploads/2023/09/05/13_ZvhhPL9.jfif',
+  },
+  {
+    name: 'Турдалиев Акылбек Беримкулович',
+    role: 'Первый заместитель Председателя Правления',
+    imageUrl:
+      'https://www.rsk.kg/media/uploads/2023/09/05/14_f6Ez1Jr.jfif',
+  },
+  {
+    name: 'Шакенов Шамабек Макейевич',
+    role: 'Менеджер Управления Комплаенс контроля',
+    imageUrl:
+      'https://www.rsk.kg/media/uploads/2023/09/05/12_pVRFId5.jfif',
+  },
+  {
+    name: 'Жаманкулова Гульнара Усенкуловна',
+    role: 'Ведущий специалист по обслуживанию клиентов',
+    imageUrl:
+      'https://www.rsk.kg/media/uploads/2023/09/05/1.jfif',
+  },
+  {
+    name: 'Тыналиева Тамара Жанузаковна',
+    role: 'Заместитель директора-юрист',
+    imageUrl:
+      'https://www.rsk.kg/media/uploads/2023/09/05/2.jfif',
+  },
+  {
+    name: 'Женишбеков Ороз',
+    role: 'BACK',
     imageUrl:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
-    name: 'Leslie Alexander',
-    role: 'Co-Founder / CEO',
+    name: 'Акылбеков Нурислам',
+    role: 'FRONT',
     imageUrl:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
-    name: 'Leslie Alexander',
-    role: 'Co-Founder / CEO',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Leslie Alexander',
-    role: 'Co-Founder / CEO',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Leslie Alexander',
-    role: 'Co-Founder / CEO',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Leslie Alexander',
-    role: 'Co-Founder / CEO',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Leslie Alexander',
-    role: 'Co-Founder / CEO',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Leslie Alexander',
-    role: 'Co-Founder / CEO',
+    name: 'Элдос Алмаз уулу',
+    role: 'FRONT',
     imageUrl:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   // More people...
 ]
 const includedFeatures = [
-  'Private forum access',
-  'Member resources',
-  'Entry to annual conference',
-  'Official member t-shirt',
+  'Индивидуальный подход',
+  'Круглосуточное обслуживание',
+  'Надежда и вера',
+  'Мы сильно хотим спать',
 ]
 function App() {
   const dispatch = useDispatch();
@@ -306,7 +306,7 @@ function App() {
                 ОАО «РСК Банк» предлагает продукты Visa Gold Eсоnomy и Visa Gold PCL. Данные карты относятся к серии карт VISA Gold и обладают присущими данной категории привилегиями.
               </p>
               <div className="mt-10 flex items-center gap-x-4">
-                <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">What’s included</h4>
+                <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">Что входит</h4>
                 <div className="h-px flex-auto bg-gray-100" />
               </div>
               <ul
@@ -324,19 +324,19 @@ function App() {
             <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
               <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
                 <div className="mx-auto max-w-xs px-8">
-                  <p className="text-base font-semibold text-gray-600">Pay once, own it forever</p>
+                  <p className="text-base font-semibold text-gray-600">Годовое обслуживание</p>
                   <p className="mt-6 flex items-baseline justify-center gap-x-2">
-                    <span className="text-5xl font-bold tracking-tight text-gray-900">$349</span>
-                    <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">USD</span>
+                    <span className="text-5xl font-bold tracking-tight text-gray-900">2000</span>
+                    <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">SOM</span>
                   </p>
                   <a
                     href="#"
                     className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
-                    Get access
+                    Приобрести карту
                   </a>
                   <p className="mt-6 text-xs leading-5 text-gray-600">
-                    Invoices and receipts available for easy company reimbursement
+                    Счета-фактуры и квитанции доступны для удобного возмещения расходов компании.
                   </p>
                 </div>
               </div>
@@ -349,10 +349,9 @@ function App() {
       <div className="bg-white py-24 sm:py-32">
         <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
           <div className="max-w-2xl">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our leadership</h2>
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Лучшие соотрудники месяца</h2>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Libero fames augue nisl porttitor nisi, quis. Id ac elit odio vitae elementum enim vitae ullamcorper
-              suspendisse.
+                  Здесь мы показываем наших лучших соотрудников которые смогли проявить себя на том или ином этапе своей карьеры 
             </p>
           </div>
           <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
